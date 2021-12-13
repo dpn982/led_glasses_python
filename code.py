27711dc7141e835319be128f62efe92ceaf38103
Dave Nichols <dpn982@gmail.com>
2021-12-13 01:00:12 -0500

    Fix Code To Work With Flutter App.

--- code.py
+++ code.py
@@ -9,7 +9,20 @@ from adafruit_ble.services.nordic import UARTService
 from adafruit_bluefruit_connect.packet import Packet
 from adafruit_bluefruit_connect.color_packet import ColorPacket
 
+colorArr1 = []
+colorArr2 = []
+BLACK = [0,0,0]
 i2c = I2C(board.SCL, board.SDA, frequency=1000000)
+
+def calculateIndex(value):
+    value = value - 1
+    return value
+
+def calculateColor(value, color1, color2):
+    if (value % 2):
+        return color1
+    else:
+        return color2
 
 # Initialize the IS31 LED driver, buffered for smoother animation
 glasses = LED_Glasses(i2c, allocate=adafruit_is31fl3741.MUST_BUFFER)
@@ -21,43 +34,46 @@ uart = UARTService()
 advertisement = ProvideServicesAdvertisement(uart)
 
 while True:
-        ble.start_advertising(advertisement)
-        while not ble.connected:
-            pass
-        ble.stop_advertising()
+    ble.start_advertising(advertisement)
+    while not ble.connected:
+        pass
+    ble.stop_advertising()
 
-        while ble.connected:
-            if uart.in_waiting:
-                raw_bytes = uart.read(uart.in_waiting)
-                try:
-                    packet = Packet.from_bytes(raw_bytes)
-                    if isinstance(packet, ColorPacket):
-                        rgb_int = packet.color[0] << 16 | packet.color[1] << 8 | packet.color[2]
-                        print ("RED: " + str(packet.color[0]))
-                        print ("GREEN:" + str(packet.color[1]))
-                        print ("BLUE: " + str(packet.color[2]))
-                        print("RGB: " + str(rgb_int))
-                        glasses.right_ring.fill(rgb_int)
-                        glasses.left_ring.fill(rgb_int)
-                        glasses.show()
-                except ValueError:
-                    text = raw_bytes.decode().strip()
-                    print("RX:", text)
-                    if (text.startswith("COLOR=")):
-                        print("COLOR FOUND")
-                        colorArr = text.replace("COLOR=", "").split(",")
-                        print ("RED: " + str(colorArr[1]))
-                        print ("GREEN: " + str(colorArr[2]))
-                        print ("BLUE: " + str(colorArr[3]))
-                        rgb_int = int(colorArr[1]) << 16 | int(colorArr[2]) << 8 | int(colorArr[3])
+    while ble.connected:
+        if uart.in_waiting:
+            raw_bytes = uart.read(uart.in_waiting)
+            try:
+                packet = Packet.from_bytes(raw_bytes)
+                if isinstance(packet, ColorPacket):
+                    rgb_int = packet.color[0] << 16 | packet.color[1] << 8 | packet.color[2]
+                    print ("RED: " + str(packet.color[0]))
+                    print ("GREEN:" + str(packet.color[1]))
+                    print ("BLUE: " + str(packet.color[2]))
+                    print("RGB: " + str(rgb_int))
+                    glasses.right_ring.fill(rgb_int)
+                    glasses.left_ring.fill(rgb_int)
+                    glasses.show()
+            except ValueError:
+                text = raw_bytes.decode().strip()
+                print("RX:", text)
+                if (text.startswith("COLOR1=")):
+                    print("COLOR FOUND")
+                    colorsArr = text.replace("COLOR1=", "")
+                    colorArr1 = colorsArr.split(",")
+                    #print("COLOR: " + colorArr1)
+                elif (text.startswith("COLOR2=")):
+                    print("COLOR FOUND")
+                    colorsArr = text.replace("COLOR2=", "")
+                    colorArr2 = colorsArr.split(",")
+                    #print("COLOR: " + colorArr1)
+                elif (text.startswith("TEXT=")):
+                    print("TEXT FOUND")
+                elif (text == "SHOW"):
+                    rgb_int1 = int(colorArr1[0]) << 16 | int(colorArr1[1]) << 8 | int(colorArr1[2])
+                    rgb_int2 = int(colorArr2[0]) << 16 | int(colorArr2[1]) << 8 | int(colorArr2[2])
 
-                        if (int(colorArr[0]) == 1):
-                            print("COLOR 1")
-                            glasses.left_ring.fill(rgb_int)
-                        else:
-                            print("COLOR 2")
-                            glasses.right_ring.fill(rgb_int)
-                    elif (text.startswith("TEXT=")):
-                        print("TEXT FOUND")
-                    elif (text == "SHOW"):
-                        glasses.show()
+                    for i in range(1,25):
+                        glasses.left_ring[calculateIndex(i)] = calculateColor(i, rgb_int1, rgb_int2)
+                        glasses.right_ring[calculateIndex(i)] = calculateColor(i, rgb_int1, rgb_int2)
+                    glasses.show()
+                    
