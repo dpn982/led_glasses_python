0d1a0786852f863d589afe8756f71dc1b4910508
Dave Nichols <dpn982@gmail.com>
2022-10-24 23:36:14 -0400

    Update libraries.

--- code.py
+++ code.py
@@ -11,6 +11,9 @@ from adafruit_bluefruit_connect.color_packet import ColorPacket
 
 colorArr1 = []
 colorArr2 = []
+rgb_int1 = -1
+rgb_int2 = -1
+OFFSET = 0
 BLACK = [0,0,0]
 i2c = I2C(board.SCL, board.SDA, frequency=1000000)
 
@@ -19,10 +22,17 @@ def calculateIndex(value):
     return value
 
 def calculateColor(value, color1, color2):
-    if (value % 2):
-        return color1
+    if OFFSET == 0:
+        ca = color1
+        cb = color2
     else:
-        return color2
+        ca = color2
+        cb = color1
+
+    if (value % 2):
+        return ca
+    else:
+        return cb
 
 # Initialize the IS31 LED driver, buffered for smoother animation
 glasses = LED_Glasses(i2c, allocate=adafruit_is31fl3741.MUST_BUFFER)
@@ -39,41 +49,50 @@ while True:
         pass
     ble.stop_advertising()
 
-    while ble.connected:
-        if uart.in_waiting:
-            raw_bytes = uart.read(uart.in_waiting)
-            try:
-                packet = Packet.from_bytes(raw_bytes)
-                if isinstance(packet, ColorPacket):
-                    rgb_int = packet.color[0] << 16 | packet.color[1] << 8 | packet.color[2]
-                    print ("RED: " + str(packet.color[0]))
-                    print ("GREEN:" + str(packet.color[1]))
-                    print ("BLUE: " + str(packet.color[2]))
-                    print("RGB: " + str(rgb_int))
-                    glasses.right_ring.fill(rgb_int)
-                    glasses.left_ring.fill(rgb_int)
-                    glasses.show()
-            except ValueError:
-                text = raw_bytes.decode().strip()
-                print("RX:", text)
-                if (text.startswith("COLOR1=")):
-                    print("COLOR FOUND")
-                    colorsArr = text.replace("COLOR1=", "")
-                    colorArr1 = colorsArr.split(",")
-                    #print("COLOR: " + colorArr1)
-                elif (text.startswith("COLOR2=")):
-                    print("COLOR FOUND")
-                    colorsArr = text.replace("COLOR2=", "")
-                    colorArr2 = colorsArr.split(",")
-                    #print("COLOR: " + colorArr1)
-                elif (text.startswith("TEXT=")):
-                    print("TEXT FOUND")
-                elif (text == "SHOW"):
-                    rgb_int1 = int(colorArr1[0]) << 16 | int(colorArr1[1]) << 8 | int(colorArr1[2])
-                    rgb_int2 = int(colorArr2[0]) << 16 | int(colorArr2[1]) << 8 | int(colorArr2[2])
+    while True:
+        if ble.connected:
+            if uart.in_waiting:
+                raw_bytes = uart.read(uart.in_waiting)
+                try:
+                    packet = Packet.from_bytes(raw_bytes)
+                    if isinstance(packet, ColorPacket):
+                        rgb_int = packet.color[0] << 16 | packet.color[1] << 8 | packet.color[2]
+                        print ("RED: " + str(packet.color[0]))
+                        print ("GREEN:" + str(packet.color[1]))
+                        print ("BLUE: " + str(packet.color[2]))
+                        print("RGB: " + str(rgb_int))
+                        glasses.right_ring.fill(rgb_int)
+                        glasses.left_ring.fill(rgb_int)
+                        glasses.show()
+                except ValueError:
+                    text = raw_bytes.decode().strip()
+                    print("RX:", text)
+                    if (text.startswith("COLOR1=")):
+                        print("COLOR FOUND")
+                        colorsArr = text.replace("COLOR1=", "")
+                        colorArr1 = colorsArr.split(",")
+                        #print("COLOR: " + colorArr1)
+                    elif (text.startswith("COLOR2=")):
+                        print("COLOR FOUND")
+                        colorsArr = text.replace("COLOR2=", "")
+                        colorArr2 = colorsArr.split(",")
+                        #print("COLOR: " + colorArr1)
+                    elif (text.startswith("TEXT=")):
+                        print("TEXT FOUND")
+                    elif (text == "SHOW"):
+                        rgb_int1 = int(colorArr1[0]) << 16 | int(colorArr1[1]) << 8 | int(colorArr1[2])
+                        rgb_int2 = int(colorArr2[0]) << 16 | int(colorArr2[1]) << 8 | int(colorArr2[2])
 
-                    for i in range(1,25):
-                        glasses.left_ring[calculateIndex(i)] = calculateColor(i, rgb_int1, rgb_int2)
-                        glasses.right_ring[calculateIndex(i)] = calculateColor(i, rgb_int1, rgb_int2)
-                    glasses.show()
-                    
+                #uart.reset_input_buffer()
+
+        if rgb_int1 > -1 and rgb_int2 > -1:
+            for i in range(1,25):
+                glasses.left_ring[calculateIndex(i)] = calculateColor(i, rgb_int1, rgb_int2)
+                glasses.right_ring[calculateIndex(i)] = calculateColor(i, rgb_int1, rgb_int2)
+            glasses.show()
+            time.sleep(2)
+
+        if OFFSET == 0:
+            OFFSET = 1
+        else:
+            OFFSET = 0
